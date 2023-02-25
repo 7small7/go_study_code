d560079e9385efc62c1c7578880f459af72f82c1
7small7 <1005349393@qq.com>
2023-02-25 23:43:33 +0800

    更新说明文档

--- goroutine/channel2.go
+++ goroutine/channel2.go
@@ -13,13 +13,13 @@ func onlyWrite() {
 	ch1 := make(chan<- int, 1)
 	defer close(ch1)
 
-	<-ch1
+	//<-ch1
 	// invalid operation: cannot receive from send-only channel ch1 (variable of type chan<- int)
 
 }
 
 func onlyRead() {
-	ch1 := make(<-chan int, 1)
-	ch1 <- 1
+	//ch1 := make(<-chan int, 1)
+	//ch1 <- 1
 	// invalid operation: cannot send to receive-only channel ch1 (variable of type <-chan int)
 }
